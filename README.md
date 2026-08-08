ae858799a6ced98d051638efc3c6136d3aab9d5a
soypat <graded.sp@gmail.com>
2026-08-08 12:45:15 -0700

    add markit example

---
+++ README.md
@@ -0,0 +1,9 @@
+# ohimark
+You are tearing me apart, Markdown libraries of the Go ecosystem.
+
+I want something that is lightweight for simple parsing. Have not thought of extensibility yet but am open to breaking changes if the API is intuitive and easy to use.
+
+
+
+## AI notice
+This package was designed by a human and assisted by an AI. We follow Oxide's RFD on LLM usage: https://rfd.shared.oxide.computer/rfd/0576 
---
+++ examples/justmarkit/markit.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"time"
+
+	"github.com/soypat/ohimark"
+)
+
+type Flags struct {
+	Input string // If empty use stdin.
+	// Output string // If empty use stdout
+}
+
+func main() {
+	var flags Flags
+	flag.StringVar(&flags.Input, "i", "", "Input markdown file. If not set is stdin")
+	flag.Parse()
+	start := time.Now()
+	err := run(flags)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		log.Fatalf("fail in %s: %s", elapsed, err)
+	}
+	log.Println("finished", elapsed)
+}
+
+func run(flags Flags) (err error) {
+	var fp io.ReaderAt
+	if flags.Input != "" {
+		fp, err = os.Open(flags.Input)
+		if err != nil {
+			return err
+		}
+	} else {
+		b, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return err
+		}
+		fp = bytes.NewReader(b)
+	}
+
+	var parser ohimark.Parser
+	err = parser.Reset(flags.Input, fp, make([]byte, 2048))
+	if err != nil {
+		return err
+	}
+	for {
+		node, err := parser.Next()
+		if err != nil {
+			return err
+		}
+		fmt.Println(node.Kind.String(), node.Len())
+	}
+}
